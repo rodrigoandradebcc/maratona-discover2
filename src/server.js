118a3db993712f80413b00832d4ee11f70052306
Rodrigo Andrade <randrade@brbatel.com.br>
2021-04-16 23:45:07 -0300

    Aula 1

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const server = express();
+const routes = require('./routes');
+
+server.set('view engine', 'ejs');
+
+server.use(express.static('public'));
+
+server.get('\\', (request, response) => {
+  return response.sendFile(__dirname + '\\views\\index.html');
+});
+
+server.use(routes);
+server.listen(3000, () => console.log('rodando'));
